fd6ade39bc11c2775699323dc04e1df13edccf65
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-03-28 16:45:40 +0000

    Done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bonnie Zhu
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates your pay depending on the amount of hours you work (including tax).
+ */
+function calculate() {
+  // input
+  const Length = parseInt(document.getElementById('Length').value)
+  const Width = parseInt(document.getElementById('Width').value)
+  const Height = parseInt(document.getElementById('Height').value)
+
+  // process
+  const Volumn = Length * Width * Height
+
+  // output
+  document.getElementById('Volumn').innerHTML = 'Volumn is: ' + Volumn + 'cm³'
+}
